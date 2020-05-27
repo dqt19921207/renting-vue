359c5ccb2f4d75d6722ac6e633a5a0c5174b4cb5
dqt <piaoxueshang@qq.com>
2020-05-28 00:27:09 +0800

    1

--- src/api/api.js
+++ src/api/api.js
@@ -4,14 +4,14 @@ let base = 'http://localhost:8080';
 
 export const requestLogin = params => { return axios.post(`${base}/renting/login`, params).then(res => res.data); };
 
-export const getUserList = params => { return axios.post(`${base}/user/list`, { params: params }); };
+export const getUserList = params => { return axios.post(`${base}/user/list`, params).then(res => res.data);};
 
-export const getUserListPage = params => { return axios.post(`${base}/user/list`, { params: params }); };
+export const getUserListPage = params => { return axios.post(`${base}/user/list`, params).then(res => res.data);};
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const removeUser = params => { return axios.post(`${base}/user/remove`,  params).then(res => res.data);};
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+export const batchRemoveUser = params => { return axios.post(`${base}/user/batchremove`, params).then(res => res.data);};
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
+export const editUser = params => { return axios.post(`${base}/user/updateUser`, params).then(res => res.data);};
 
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
+export const addUser = params => { return axios.post(`${base}/user/addUser`, params).then(res => res.data);};
--- src/views/user/userlist.vue
+++ src/views/user/userlist.vue
@@ -1,305 +1,320 @@
 <template>
-	<section>
-		<!--工具条-->
-		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
-			<el-form :inline="true" :model="filters">
-				<el-form-item>
-					<el-input v-model="filters.username" placeholder="登录号"></el-input>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" v-on:click="getUsers">查询</el-button>
-				</el-form-item>
-				<el-form-item>
-					<el-button type="primary" @click="handleAdd">新增</el-button>
-				</el-form-item>
-			</el-form>
-		</el-col>
+  <section>
+    <!--工具条-->
+    <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+      <el-form :inline="true" :model="filters">
+        <el-form-item>
+          <el-input v-model="filters.username" placeholder="登录号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" v-on:click="getUsers">查询</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleAdd">新增</el-button>
+        </el-form-item>
+      </el-form>
+    </el-col>
 
-		<!--列表-->
-		<el-table :data="users" highlight-current-row v-loading="listLoading" @selection-change="selsChange" style="width: 100%;">
-			<el-table-column type="selection">
-			</el-table-column>
-			<el-table-column type="index" >
-			</el-table-column>
-			<el-table-column prop="username" label="登录号"  >
-			</el-table-column>
-			<el-table-column prop="realname" label="用户名" >
-			</el-table-column>
-			<el-table-column prop="tel" label="电话"   >
-			</el-table-column>
-			<el-table-column prop="rolename" label="角色"  >
-			</el-table-column>
-			<el-table-column prop="del" label="启用状态" :formatter="formatDel" >
-			</el-table-column>
-			<el-table-column label="操作">
-				<template scope="scope">
-					<el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
-					<el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
-				</template>
-			</el-table-column>
-		</el-table>
+    <!--列表-->
+    <el-table
+      :data="users"
+      highlight-current-row
+      v-loading="listLoading"
+      @selection-change="selsChange"
+      style="width: 100%;"
+    >
+      <el-table-column type="selection"></el-table-column>
+      <el-table-column type="index"></el-table-column>
+      <el-table-column prop="username" label="登录号"></el-table-column>
+      <el-table-column prop="realname" label="用户名"></el-table-column>
+      <el-table-column prop="tel" label="电话"></el-table-column>
+      <el-table-column prop="rolename" label="角色"></el-table-column>
+      <el-table-column prop="del" label="启用状态" :formatter="formatDel"></el-table-column>
+      <el-table-column label="操作">
+        <template scope="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
 
-		<!--工具条-->
-		<el-col :span="24" class="toolbar">
-			<el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button>
-			<el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;">
-			</el-pagination>
-		</el-col>
+    <!--工具条-->
+    <el-col :span="24" class="toolbar">
+      <!-- <el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button> -->
+      <el-pagination
+        layout="prev, pager, next"
+        @current-change="handleCurrentChange"
+        :page-size="20"
+        :total="total"
+        style="float:right;"
+      ></el-pagination>
+    </el-col>
 
-		<!--编辑界面-->
-		<el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-			<el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="editForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="editForm.sex">
-						<el-radio class="radio" :label="1">禁用</el-radio>
-						<el-radio class="radio" :label="0">启用</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input type="textarea" v-model="editForm.addr"></el-input>
-				</el-form-item>
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="editFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
-			</div>
-		</el-dialog>
+    <!--编辑界面-->
+    <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
+      <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
+        <el-form-item label="id" hidden>
+          <el-input v-model="editForm.id" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="登录号">
+          <el-input v-model="editForm.username" disabled auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="editForm.password"  auto-complete="off"></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="重复密码">
+          <el-input v-model="editForm.password1" type="password" auto-complete="off"></el-input> -->
+        </el-form-item>
+        <el-form-item label="昵称">
+          <el-input v-model="editForm.realname" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="editForm.tel" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click.native="editFormVisible = false">取消</el-button>
+        <el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
+      </div>
+    </el-dialog>
 
-		<!--新增界面-->
-		<el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
-			<el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
-				<el-form-item label="姓名" prop="name">
-					<el-input v-model="addForm.name" auto-complete="off"></el-input>
-				</el-form-item>
-				<el-form-item label="性别">
-					<el-radio-group v-model="addForm.sex">
-						<el-radio class="radio" :label="1">男</el-radio>
-						<el-radio class="radio" :label="0">女</el-radio>
-					</el-radio-group>
-				</el-form-item>
-				<el-form-item label="年龄">
-					<el-input-number v-model="addForm.age" :min="0" :max="200"></el-input-number>
-				</el-form-item>
-				<el-form-item label="生日">
-					<el-date-picker type="date" placeholder="选择日期" v-model="addForm.birth"></el-date-picker>
-				</el-form-item>
-				<el-form-item label="地址">
-					<el-input type="textarea" v-model="addForm.addr"></el-input>
-				</el-form-item>
-			</el-form>
-			<div slot="footer" class="dialog-footer">
-				<el-button @click.native="addFormVisible = false">取消</el-button>
-				<el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
-			</div>
-		</el-dialog>
-	</section>
+    <!--新增界面-->
+    <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
+      <el-form :model="addForm" label-width="80px" :rules="addFormRules" ref="addForm">
+        <el-form-item label="登录号">
+          <el-input v-model="addForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="addForm.password"   auto-complete="off"></el-input>
+        </el-form-item>
+        <!-- <el-form-item label="重复密码">
+          <el-input v-model="addForm.password1" type="password" auto-complete="off"></el-input> -->
+        </el-form-item>
+        <el-form-item label="昵称">
+          <el-input v-model="addForm.realname" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="电话">
+          <el-input v-model="addForm.tel" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click.native="addFormVisible = false">取消</el-button>
+        <el-button type="primary" @click.native="addSubmit" :loading="addLoading">提交</el-button>
+      </div>
+    </el-dialog>
+  </section>
 </template>
 
 <script>
-	import util from '../../common/js/util'
-	//import NProgress from 'nprogress'
-	import { getUserListPage, removeUser, batchRemoveUser, editUser, addUser } from '../../api/api';
+import util from "../../common/js/util";
+//import NProgress from 'nprogress'
+import {
+  getUserListPage,
+  removeUser,
+  batchRemoveUser,
+  editUser,
+  addUser
+} from "../../api/api";
 
-	export default {
-		data() {
-			return {
-				filters: {
-					name: ''
-				},
-				users: [],
-				total: 0,
-				page: 1,
-				listLoading: false,
-				sels: [],//列表选中列
+export default {
+  data() {
+    return {
+      filters: {
+        name: ""
+      },
+      users: [],
+      total: 0,
+      page: 1,
+      listLoading: false,
+      sels: [], //列表选中列
 
-				editFormVisible: false,//编辑界面是否显示
-				editLoading: false,
-				editFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//编辑界面数据
-				editForm: {
-					id: 0,
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				},
+      editFormVisible: false, //编辑界面是否显示
+      editLoading: false,
+      editFormRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }]
+      },
+      //编辑界面数据
+      editForm: {
+        id: 0,
+        name: "",
+        sex: -1,
+        age: 0,
+        birth: "",
+        addr: ""
+      },
 
-				addFormVisible: false,//新增界面是否显示
-				addLoading: false,
-				addFormRules: {
-					name: [
-						{ required: true, message: '请输入姓名', trigger: 'blur' }
-					]
-				},
-				//新增界面数据
-				addForm: {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				}
+      addFormVisible: false, //新增界面是否显示
+      addLoading: false,
+      addFormRules: {
+        name: [{ required: true, message: "请输入姓名", trigger: "blur" }]
+      },
+      //新增界面数据
+      addForm: {
+        name: "",
+        sex: -1,
+        age: 0,
+        birth: "",
+        addr: ""
+      }
+    };
+  },
+  methods: {
+    //禁用显示转换
+    formatDel: function(row, column) {
+      return row.del == 1 ? "禁用" : row.del == 0 ? "启用" : "未知";
+    },
+    handleCurrentChange(val) {
+      this.page = val;
+      this.getUsers();
+    },
+    //获取用户列表
+    getUsers() {
+      let para = {
+        page: this.page,
+        username: this.filters.username
+      };
+      this.listLoading = true;
+      //NProgress.start();
+      // console.log(para)
+      getUserListPage(para).then(res => {
+        console.log(res)
+        this.total = res.datas.total;
+        this.users = res.datas.list;
+        this.listLoading = false;
+      });
+    },
+    //删除
+    handleDel: function(index, row) {
+      this.$confirm("确认删除该记录吗?", "提示", {
+        type: "warning"
+      })
+        .then(() => {
+          this.listLoading = true;
+          //NProgress.start();
+          let para = { id: row.id };
+          removeUser(para).then(res => {
+            this.listLoading = false;
+            //NProgress.done();
+            this.$message({
+              message: "删除成功",
+              type: "success"
+            });
+            this.getUsers();
+          });
+        })
+        .catch(() => {});
+    },
+    //显示编辑界面
+    handleEdit: function(index, row) {
+      this.editFormVisible = true;
+      this.editForm = Object.assign({}, row);
+    },
+    //显示新增界面
+    handleAdd: function() {
+      this.addFormVisible = true;
+      this.addForm = {
+        username: "",
+        password: "",
+        // realname: "",
+        tel: ""
+      };
+    },
+    //编辑
+    editSubmit: function() {
+      this.$refs.editForm.validate(valid => {
+        if (valid) {
+          this.$confirm("确认提交吗？", "提示", {}).then(() => {
+            this.editLoading = true;
+            //NProgress.start();
+            let para = Object.assign({}, this.editForm);
+            para.birth =
+              !para.birth || para.birth == ""
+                ? ""
+                : util.formatDate.format(new Date(para.birth), "yyyy-MM-dd");
+            editUser(para).then(res => {
+              this.editLoading = false;
+              //NProgress.done();
+              this.$message({
+                message: "提交成功",
+                type: "success"
+              });
+              this.$refs["editForm"].resetFields();
+              this.editFormVisible = false;
+              this.getUsers();
+            });
+          });
+        }
+      });
+    },
+    //新增
+    addSubmit: function() {
+      this.$refs.addForm.validate(valid => {
+        if (valid) {
+          this.$confirm("确认提交吗？", "提示", {}).then(() => {
+            this.addLoading = true;
+            //NProgress.start();
+            let para = Object.assign({}, this.addForm);
+              console.log(para);
+            // if (para.password != para.password1) {
+            //   alert("两次密码输入不一致！");
+            //   return;
+            // }
+            addUser(para).then(res => {
+              console.log(res);
+              this.addLoading = false;
+              //NProgress.done();
 
-			}
-		},
-		methods: {
-			//性别显示转换
-			formatDel: function (row, column) {
-				return row.del == 1 ? '禁用' : row.del == 0 ? '启用' : '未知';
-			},
-			handleCurrentChange(val) {
-				this.page = val;
-				this.getUsers();
-			},
-			//获取用户列表
-			getUsers() { 
-				let para = {
-					page: this.page,
-					username: this.filters.username
-				};
-				this.listLoading = true;
-				//NProgress.start();
-					console.log(para)
-				getUserListPage(para).then((res) => {
-					console.log(res) 
-					this.total = res.data.datas.total;
-					this.users = res.data.datas.list;
-					console.log(this.users)
-					this.listLoading = false; 
-				});
-			},
-			//删除
-			handleDel: function (index, row) {
-				this.$confirm('确认删除该记录吗?', '提示', {
-					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { id: row.id };
-					removeUser(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getUsers();
-					});
-				}).catch(() => {
-
-				});
-			},
-			//显示编辑界面
-			handleEdit: function (index, row) {
-				this.editFormVisible = true;
-				this.editForm = Object.assign({}, row);
-			},
-			//显示新增界面
-			handleAdd: function () {
-				this.addFormVisible = true;
-				this.addForm = {
-					name: '',
-					sex: -1,
-					age: 0,
-					birth: '',
-					addr: ''
-				};
-			},
-			//编辑
-			editSubmit: function () {
-				this.$refs.editForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.editLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.editForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							editUser(para).then((res) => {
-								this.editLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['editForm'].resetFields();
-								this.editFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			//新增
-			addSubmit: function () {
-				this.$refs.addForm.validate((valid) => {
-					if (valid) {
-						this.$confirm('确认提交吗？', '提示', {}).then(() => {
-							this.addLoading = true;
-							//NProgress.start();
-							let para = Object.assign({}, this.addForm);
-							para.birth = (!para.birth || para.birth == '') ? '' : util.formatDate.format(new Date(para.birth), 'yyyy-MM-dd');
-							addUser(para).then((res) => {
-								this.addLoading = false;
-								//NProgress.done();
-								this.$message({
-									message: '提交成功',
-									type: 'success'
-								});
-								this.$refs['addForm'].resetFields();
-								this.addFormVisible = false;
-								this.getUsers();
-							});
-						});
-					}
-				});
-			},
-			selsChange: function (sels) {
-				this.sels = sels;
-			},
-			//批量删除
-			batchRemove: function () {
-				var ids = this.sels.map(item => item.id).toString();
-				this.$confirm('确认删除选中记录吗？', '提示', {
-					type: 'warning'
-				}).then(() => {
-					this.listLoading = true;
-					//NProgress.start();
-					let para = { ids: ids };
-					batchRemoveUser(para).then((res) => {
-						this.listLoading = false;
-						//NProgress.done();
-						this.$message({
-							message: '删除成功',
-							type: 'success'
-						});
-						this.getUsers();
-					});
-				}).catch(() => {
-
-				});
-			}
-		},
-		mounted() {
-			this.getUsers();
-		}
-	}
-
+              let { message, code, datas } = res;
+              if (code !== '100000') {
+                this.$message({
+                  message: message,
+                  type: "error"
+                });
+              } else {
+                this.$message({
+                  message: "提交成功",
+                  type: "success"
+                });
+                this.$refs["addForm"].resetFields();
+                this.addFormVisible = false;
+                this.getUsers();
+              }
+            });
+          });
+        }
+      });
+    },
+    selsChange: function(sels) {
+      this.sels = sels;
+    },
+    //批量删除
+    batchRemove: function() {
+      var ids = this.sels.map(item => item.id).toString();
+      this.$confirm("确认删除选中记录吗？", "提示", {
+        type: "warning"
+      })
+        .then(() => {
+          this.listLoading = true;
+          //NProgress.start();
+          let para = { ids: ids };
+          batchRemoveUser(para).then(res => {
+            this.listLoading = false;
+            //NProgress.done();
+            this.$message({
+              message: "删除成功",
+              type: "success"
+            });
+            this.getUsers();
+          });
+        })
+        .catch(() => {});
+    }
+  },
+  mounted() {
+    this.getUsers();
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
